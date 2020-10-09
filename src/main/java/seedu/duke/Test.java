4439007bf2cbd054cd82cc511bf343608834dc6a
Aliciaho <E0425365@u.nus.edu>
2020-10-09 20:09:11 +0800

    did the Add test Feature

---
+++ src/main/java/seedu/duke/Test.java
@@ -0,0 +1,19 @@
+package seedu.duke;
+
+public class Test extends Event{
+
+    protected String start;
+    protected String end;
+
+    public Test(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[TEST] " + super.description + "from " + start
+                + "to " + end + " has been added.";
+    }
+}
