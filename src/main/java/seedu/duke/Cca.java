0a07ab9bb6c9cb53cbb45fc5f7953c17742a76dc
chanx <chanxuhui@outlook.com>
2020-10-10 21:54:46 +0800

    Add new constructor to Cca class Add getter methods for Cca parameters Add getTypeIcon() method for Cca class

--- src/main/java/seedu/duke/Cca.java
+++ src/main/java/seedu/duke/Cca.java
@@ -11,6 +11,24 @@ public class Cca extends Event {
         this.end = end;
     }
 
+    public Cca(String description, boolean isDone, String start, String end) {
+        super(description, isDone);
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getStart () {
+        return start;
+    }
+
+    public String getEnd () {
+        return end;
+    }
+
+    public String getTypeIcon() {
+        return "[CCA]";
+    }
+
     @Override
     public String toString() {
         return "[CCA] " + super.description + "from " + start
