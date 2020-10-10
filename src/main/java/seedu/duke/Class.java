7e2be6308254c6a71142f5c990475cf78ad5d079
chanx <chanxuhui@outlook.com>
2020-10-10 21:56:28 +0800

    Add new constructor to Class class Add getter methods for Class parameters Add getTypeIcon() method for Class class

--- src/main/java/seedu/duke/Class.java
+++ src/main/java/seedu/duke/Class.java
@@ -5,10 +5,28 @@ public class Class extends Event {
     protected String start;
     protected String end;
 
-    public Class(String description, String start, String end) {
+    public Class (String description, String start, String end) {
         super(description);
         this.start = start;
         this.end = end;
+    }
+
+    public Class (String description, boolean isDone, String start, String end) {
+        super(description, isDone);
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getStart () {
+        return start;
+    }
+
+    public String getEnd () {
+        return end;
+    }
+
+    public String getTypeIcon() {
+        return "[CLASS]";
     }
 
     @Override
