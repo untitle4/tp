c23a9a44b7f520357943a72d0456ba121a419354
untitle4 <e0376922@u.nus.edu>
2020-10-11 17:22:02 +0800

    change JUnit test output

--- src/test/java/seedu/duke/ListScheduleTest.java
+++ src/test/java/seedu/duke/ListScheduleTest.java
@@ -24,7 +24,7 @@ class ListScheduleTest {
         ListSchedule listSchedule = new ListSchedule(classes, new ArrayList<>(), new ArrayList<>());
         ArrayList<String> actualOutputs = listSchedule.getAllEventsPrinted();
         ArrayList<String> expectedOutputs = new ArrayList<>(
-                List.of("Classes:", "1. [Class] Math from 26/2/2019 to 27/2/2019",  "CCAs:", "Tests:"));
+                List.of("Classes:", "1. [Class] [NOT DONE] Math from 26/2/2019 to 27/2/2019",  "CCAs:", "Tests:"));
         assertEquals(actualOutputs, expectedOutputs);
     }
 }
