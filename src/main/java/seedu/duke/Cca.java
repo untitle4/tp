3b905a8be42db1be1ddc15e62c7c560aaa48c797
chanx <chanxuhui@outlook.com>
2020-10-12 01:27:37 +0800

    Update Cca.java

--- src/main/java/seedu/duke/Cca.java
+++ src/main/java/seedu/duke/Cca.java
@@ -31,7 +31,7 @@ public class Cca extends Event {
 
     @Override
     public String toString() {
-        return "[CCA] " + super.getDescription() + "from " + start
+        return "[CCA] " + super.toString() + "from " + start
                 + "to " + end;
     }
 
