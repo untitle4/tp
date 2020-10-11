284babfc388c19931137f23f4c645e767b09765a
AndreWongZH <wongandre98@gmail.com>
2020-10-11 15:31:50 +0800

    Refactor command parse
    Lets create a command parser class so that we can have it extract
    out the commands from the user ui and then execute them.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,5 +1,8 @@
 package seedu.duke;
 
+import seedu.duke.parser.CommandParser;
+import seedu.duke.parser.CommandType;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -43,65 +46,17 @@ public class Duke {
         System.out.println("Hello " + in.nextLine());
         System.out.println("What can we do for you?");
         String line;
+        CommandType commandType = null;
         Scanner input = new Scanner(System.in);
-        while (input.hasNextLine()) {
+        while (input.hasNextLine() && commandType != CommandType.BYE) {
             line = input.nextLine();
-
-            String[] checkCommand = line.split(" ");
-            if (checkCommand[0].equals("help")) {
-                try {
-                    handleHelp(checkCommand);
-                } catch (InvalidHelpCommandException e) {
-                    System.out.println("Oops! If you're trying to ask for help, simply enter 'help'!\n");
-                }
-            } else if (checkCommand[0].equals("add") && checkCommand[1].equals("class")) {
-                classManager.addClass(line);
-            } else if (checkCommand[0].equals("add") && checkCommand[1].equals("test")) {
-                testManager.addTest(line);
-            } else if (checkCommand[0].equals("add") && checkCommand[1].equals("cca")) {
-                ccaManager.addCca(line);
-            } else if (checkCommand[0].equals("delete") && checkCommand[1].equals("class")) {
-                classManager.deleteClass(checkCommand);
-            } else if (checkCommand[0].equals("delete") && checkCommand[1].equals("test")) {
-                testManager.deleteTest(checkCommand);
-            } else if (checkCommand[0].equals("delete") && checkCommand[1].equals("cca")) {
-                ccaManager.deleteCca(checkCommand);
-            } else if (checkCommand[0].equals("list")) {
-                ArrayList<String> printedEvents = listSchedule.getAllEventsPrinted();
-                printArray(printedEvents);
-            } else if (checkCommand[0].equals("bye")) {
-                break;
-            }
+            commandType = new CommandParser(line, testManager, ccaManager, classManager, listSchedule)
+                    .parseCommand();
             refreshEvents();
         }
 
         //Exit Message
         System.out.println("BYE BYE! SEE YOU NEXT TIME! :3");
-    }
-
-    private static void printArray(ArrayList<String> printedEvents) {
-        for (String line : printedEvents) {
-            System.out.println(line);
-        }
-    }
-
-    public static void handleHelp(String[] checkCommand) throws InvalidHelpCommandException {
-        if (checkCommand.length == 1) {
-            System.out.println("Hello! Here is a list of commands you can try:\n\n"
-                    + "\t1. Add class: add class /n [name of class] /s [start date-time of class] /e"
-                    + " [end date-time of class]\n"
-                    + "\t2. Delete class: delete class /n [class number]\n"
-                    + "\t3. Add cca: add cca /n [name of cca] /s [start date-time of cca] /e [end date-time of cca]\n"
-                    + "\t4. Delete cca: type delete cca /n [cca number]\n"
-                    + "\t5. Add test: type add test /n [name of test] /s [start date-time of test] /e "
-                    + "[end date-time of test]\n"
-                    + "\t6. Delete test: type delete test /n [test number]\n"
-                    + "\t7. Delete all: delete all\n");
-        // "\n\tPlease enter the date-time in the following format: YYYY-MM-DD [time in 24hr format]\n" +
-        // "\te.g. 2020-08-19 1300\n\n);
-        } else {
-            throw new InvalidHelpCommandException();
-        }
     }
 
     public static class InvalidHelpCommandException extends Exception {
