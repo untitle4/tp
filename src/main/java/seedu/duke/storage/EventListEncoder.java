ef45cd46e497ff99b1756c8817c3669c636c3cdd
chanx <chanxuhui@outlook.com>
2020-10-12 22:53:28 +0800

    Reworked logic of EventListEncoder due to protected access

--- src/main/java/seedu/duke/storage/EventListEncoder.java
+++ src/main/java/seedu/duke/storage/EventListEncoder.java
@@ -28,21 +28,21 @@ public class EventListEncoder {
             Cca cca = (Cca) event;
             result = cca.getTypeIcon() + "|"
                     + cca.isDone() + "|"
-                    + cca.description + "|"
+                    + cca.getDescription() + "|"
                     + cca.getStart() + "|"
                     + cca.getEnd();
         } else if (event instanceof Test) {
             Test test = (Test) event;
             result = test.getTypeIcon() + "|"
                     + test.isDone() + "|"
-                    + test.description + "|"
+                    + test.getDescription() + "|"
                     + test.getStart() + "|"
                     + test.getEnd();
         } else if (event instanceof Class) {
             Class newClass = (Class) event;
             result = newClass.getTypeIcon() + "|"
                     + newClass.isDone() + "|"
-                    + newClass.description + "|"
+                    + newClass.getDescription() + "|"
                     + newClass.getStart() + "|"
                     + newClass.getEnd();
         }
