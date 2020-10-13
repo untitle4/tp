4299243053be55cb617ee078e7a6e4583bc39547
Elizabeth Chan <elizabethchan1308@gmail.com>
2020-10-13 13:56:28 +0800

    Commit to enable pull

--- src/main/java/seedu/duke/ClassManager.java
+++ src/main/java/seedu/duke/ClassManager.java
@@ -1,6 +1,9 @@
 package seedu.duke;
 
+import java.io.InvalidClassException;
 import java.util.ArrayList;
+import seedu.duke.exception.InvalidClassInputException;
+import seedu.duke.exception.TestParamException;
 
 public class ClassManager {
     private final ArrayList<Event> classes;
@@ -17,28 +20,48 @@ public class ClassManager {
         return classes.size();
     }
 
-    public void addClass(String userInput) {
-        final String[] classDetails = userInput.trim().split("\\/");
+    public void addClass(String userInput) throws InvalidClassInputException {
 
-        String classDescription = classDetails[1].substring(2);
-        String classStartDate = classDetails[2].substring(2);
-        String classEndDate = classDetails[3].substring(2);
+        if ((!userInput.contains("/n")) || (!userInput.contains("/s")) || (!userInput.contains("/e"))) {
+            throw new InvalidClassInputException();
+        }
 
-        classes.add(new Class(classDescription, classStartDate, classEndDate));
+        try {
+            final String[] classDetails = userInput.trim().split("\\/");
 
-        System.out.println("Got it. I've added this class: ");
-        System.out.println(classes.get(getClassListSize() - 1));
-        getClassStatement();
+            String classDescription = classDetails[1].substring(2);
+            String classStartDate = classDetails[2].substring(2);
+            String classEndDate = classDetails[3].substring(2);
+
+            classes.add(new Class(classDescription, classStartDate, classEndDate));
+
+            System.out.println("Got it. I've added this class: ");
+            System.out.println(classes.get(getClassListSize() - 1));
+            getClassStatement();
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new InvalidClassInputException();
+        }
     }
 
     public void deleteClass(String[] userInput) {
-        int classIndex = Integer.parseInt(userInput[2]);
+        try {
+            int classIndex = Integer.parseInt(userInput[2]);
 
-        System.out.println("Noted. I've removed this class: ");
-        System.out.println(classes.get(classIndex - 1));
+            // Just to test if class index is valid - for exception use only
+            classes.get(classIndex-1);
 
-        classes.remove(classIndex - 1);
-        getClassStatement();
+            System.out.println("Noted. I've removed this class: ");
+            System.out.println(classes.get(classIndex - 1));
+
+            classes.remove(classIndex - 1);
+            getClassStatement();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("OOPS! Please indicate which class you'd like to delete");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("OOPS! Please indicate a valid class index!");
+        } catch (NumberFormatException e) {
+            System.out.println("OOPS! Please indicate in NUMERALS, which class you'd like to delete!");
+        }
     }
 
     private void getClassStatement() {
