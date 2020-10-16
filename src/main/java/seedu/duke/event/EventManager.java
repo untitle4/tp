3240d1e56da84c1d1714d73ac3a15018cd56d5ae
chanx <chanxuhui@outlook.com>
2020-10-16 15:39:02 +0800

    Add Tuition to EventManager

--- src/main/java/seedu/duke/event/EventManager.java
+++ src/main/java/seedu/duke/event/EventManager.java
@@ -1,9 +1,6 @@
 package seedu.duke.event;
 
-import seedu.duke.ClassManager;
-import seedu.duke.TestManager;
-import seedu.duke.CcaManager;
-import seedu.duke.ListSchedule;
+import seedu.duke.*;
 import seedu.duke.exception.EmptyListException;
 
 import java.util.ArrayList;
@@ -12,11 +9,13 @@ public class EventManager {
     private static ClassManager classManager;
     private static TestManager testManager;
     private static CcaManager ccaManager;
+    private static TuitionManager tuitionManager;
 
     public EventManager(EventParameter eventParameter) {
         classManager = new ClassManager(eventParameter.getClasses());
         testManager = new TestManager(eventParameter.getTests());
         ccaManager = new CcaManager(eventParameter.getCcas());
+        tuitionManager = new TuitionManager(new ArrayList<>());
     }
 
     public ClassManager getClassManager() {
@@ -31,10 +30,12 @@ public class EventManager {
         return ccaManager;
     }
 
+    public TuitionManager getTuitionManager() { return tuitionManager; }
+
     public void listSchedule() {
         try {
             ListSchedule listSchedule = new ListSchedule(
-                classManager.getClasses(), ccaManager.getCcaList(), testManager.getTestList());
+                classManager.getClasses(), ccaManager.getCcaList(), testManager.getTestList(), tuitionManager.getTuitions());
             ArrayList<String> printedEvents = listSchedule.getAllEventsPrinted();
             printArray(printedEvents);
         } catch (EmptyListException e) {
