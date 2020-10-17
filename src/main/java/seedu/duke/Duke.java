103eee4b78f56b4f712d05f61d4781c42ede8192
AndreWongZH <wongandre98@gmail.com>
2020-10-17 23:38:41 +0800

    Refactor Duke class to use ControlManager class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -2,10 +2,10 @@ package seedu.duke;
 
 import seedu.duke.common.Messages;
 import seedu.duke.contact.ContactManager;
+import seedu.duke.controller.ControlManager;
 import seedu.duke.event.EventManager;
 import seedu.duke.event.EventParameter;
-import seedu.duke.parser.CommandParser;
-import seedu.duke.parser.CommandType;
+import seedu.duke.controller.parser.CommandType;
 import seedu.duke.quiz.Quiz;
 import seedu.duke.quiz.QuizManager;
 import seedu.duke.storage.QuizStorageManager;
@@ -21,33 +21,31 @@ public class Duke {
 
     private static EventStorageManager eventStorageManager;
     private static QuizStorageManager quizStorageManager;
-    private static EventManager eventManager;
-    private static QuizManager quizManager;
-    private static ContactManager contactManager;
     private static UserInterface userInterface;
     private static boolean active = true;
+    private static Model model;
 
     /**
      * Main entry-point for the java.duke.Duke application.
      */
-
     public static void main(String[] args) {
         new LogManager();
         eventStorageManager = new EventStorageManager(EVENT_FILE_NAME);
         quizStorageManager = new QuizStorageManager(QUIZ_FILE_NAME);
         EventParameter eventParameter = eventStorageManager.loadData();
-        eventManager = new EventManager(eventParameter);
-        quizManager = new QuizManager(quizStorageManager.loadData());
-        contactManager = new ContactManager();
+        EventManager eventManager = new EventManager(eventParameter);
+        QuizManager quizManager = new QuizManager(quizStorageManager.loadData());
+        ContactManager contactManager = new ContactManager();
         userInterface = UserInterface.getInstance();
+        model = new Model(eventManager, contactManager, quizManager);
 
         userInterface.showWelcomeMessage();
 
         while (active) {
             String line = userInterface.getUserCommand();
             if (!line.trim().isEmpty()) {
-                CommandType commandType = new CommandParser(line, eventManager,
-                        quizManager, contactManager).parseCommand();
+                ControlManager controlManager = new ControlManager(line, model);
+                CommandType commandType = controlManager.runLogic();
                 checkIfProgramEnds(commandType);
             }
             refreshEvents();
@@ -67,10 +65,10 @@ public class Duke {
     private static void refreshEvents() {
         ArrayList<Event> events = new ArrayList<>();
 
-        events.addAll(eventManager.getCcaManager().getCcaList());
-        events.addAll(eventManager.getTestManager().getTestList());
-        events.addAll(eventManager.getClassManager().getClasses());
-        events.addAll(eventManager.getTuitionManager().getTuitions());
+        events.addAll(model.getEventManager().getCcaManager().getCcaList());
+        events.addAll(model.getEventManager().getTestManager().getTestList());
+        events.addAll(model.getEventManager().getClassManager().getClasses());
+        events.addAll(model.getEventManager().getTuitionManager().getTuitions());
 
         try {
             eventStorageManager.saveData(events);
@@ -80,7 +78,7 @@ public class Duke {
     }
 
     private static void refreshQuizzes() {
-        ArrayList<Quiz> quizzes = quizManager.getQuizList();
+        ArrayList<Quiz> quizzes = model.getQuizManager().getQuizList();
 
         try {
             quizStorageManager.saveData(quizzes, QUIZ_FILE_NAME);
