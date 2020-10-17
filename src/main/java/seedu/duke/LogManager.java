688b7125085a42f5df8d23ad2993b74dbb1ae897
chanx <chanxuhui@outlook.com>
2020-10-17 20:51:45 +0800

    Refactored LogManager to use UserInterface methods

--- src/main/java/seedu/duke/LogManager.java
+++ src/main/java/seedu/duke/LogManager.java
@@ -7,15 +7,18 @@ import java.util.logging.SimpleFormatter;
 
 public class LogManager {
     private static final Logger LOGGER = Logger.getLogger("log");
+    private UserInterface userInterface;
 
     public LogManager() {
+        userInterface = UserInterface.getInstance();
+
         try {
             FileHandler fileHandler = new FileHandler("./logfile.txt");
             fileHandler.setFormatter(new SimpleFormatter());
             LOGGER.setUseParentHandlers(false);
             LOGGER.addHandler(fileHandler);
         } catch (IOException e) {
-            System.out.println("Failed to set up logger");
+            userInterface.showToUser(Messages.MESSAGE_INITIALIZATION_LOGGER_FAILED);
         }
     }
 
