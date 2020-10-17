dea1951f5511d568ce79ea2e6e7409b2b9ca1abb
AndreWongZH <wongandre98@gmail.com>
2020-10-17 23:31:47 +0800

    Add controlmanager class
    The control manager class is responsible for parsing userinput,
    and then executing the command instance. It also handles all the
    execptions thrown during the executing of command.

---
+++ src/main/java/seedu/duke/controller/ControlManager.java
@@ -0,0 +1,64 @@
+package seedu.duke.controller;
+
+import seedu.duke.Model;
+import seedu.duke.controller.command.Command;
+import seedu.duke.controller.command.CommandFactory;
+import seedu.duke.controller.parser.CommandParser;
+import seedu.duke.controller.parser.CommandType;
+import seedu.duke.exception.CcaEmptyStringException;
+import seedu.duke.exception.CcaParamException;
+import seedu.duke.exception.ContactEmptyStringException;
+import seedu.duke.exception.ContactParamException;
+import seedu.duke.exception.EmptyTuitionInputException;
+import seedu.duke.exception.InvalidClassInputException;
+import seedu.duke.exception.InvalidCommandException;
+import seedu.duke.exception.InvalidHelpCommandException;
+import seedu.duke.exception.InvalidTuitionInputException;
+import seedu.duke.exception.TestEmptyStringException;
+import seedu.duke.exception.TestParamException;
+
+public class ControlManager {
+    private final String userInput;
+    private final Model model;
+
+    public ControlManager(String userInput, Model model) {
+        this.userInput = userInput;
+        this.model = model;
+    }
+
+    public CommandType runLogic() {
+        CommandType commandType = null;
+        try {
+            commandType = new CommandParser(userInput).extractCommand();
+            Command actionableCommand = new CommandFactory(commandType, userInput).generateActionableCommand();
+            if (commandType == CommandType.BYE) {
+                return commandType;
+            }
+            actionableCommand.execute(model);
+        } catch (InvalidClassInputException e) {
+            e.printStackTrace();
+        } catch (TestParamException e) {
+            e.printStackTrace();
+        } catch (TestEmptyStringException e) {
+            e.printStackTrace();
+        } catch (CcaParamException e) {
+            e.printStackTrace();
+        } catch (CcaEmptyStringException e) {
+            e.printStackTrace();
+        } catch (EmptyTuitionInputException e) {
+            e.printStackTrace();
+        } catch (InvalidTuitionInputException e) {
+            e.printStackTrace();
+        } catch (ContactEmptyStringException e) {
+            e.printStackTrace();
+        } catch (InvalidHelpCommandException e) {
+            e.printStackTrace();
+        } catch (ContactParamException e) {
+            e.printStackTrace();
+        } catch (InvalidCommandException e) {
+            System.out.println("☹ Oops! I did not recognize that command! Enter 'help' if needed!");
+        }
+
+        return commandType;
+    }
+}
