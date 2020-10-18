4021892353edb68f0f076bcc460c08a6ac888afa
AndreWongZH <wongandre98@gmail.com>
2020-10-18 14:42:14 +0800

    Add method to parse model type
    Command parser now only parse the command operand.
    The model parser is responsible for detecting which model is
    required.
    The model extractor then extracts the relavent model.

--- src/main/java/seedu/duke/controller/parser/CommandParser.java
+++ src/main/java/seedu/duke/controller/parser/CommandParser.java
@@ -12,15 +12,8 @@ public class CommandParser {
     public static final String INPUT_DONE = "done";
     public static final String INPUT_LIST = "list";
     public static final String INPUT_BYE = "bye";
-    public static final String INPUT_SCHEDULE_CLASS = "class";
-    public static final String INPUT_SCHEDULE_TEST = "test";
-    public static final String INPUT_SCHEDULE_CCA = "cca";
-    public static final String INPUT_SCHEDULE_TUITION = "tuition";
     public static final String INPUT_HELP = "help";
-    public static final String INPUT_QUIZ = "quiz";
-    public static final String INPUT_CONTACT = "contact";
     public static final int MAIN_COMMAND_INDEX = 0;
-    public static final int SUB_COMMAND_INDEX = 1;
 
     private final String[] separatedInputs;
 
@@ -33,64 +26,28 @@ public class CommandParser {
     }
 
     public CommandType extractCommand() throws InvalidCommandException {
-        logger.log(Level.INFO, "Extracting commands now...");
-        if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_HELP)) {
+        logger.log(Level.INFO, "Extracting command now...");
+
+        switch (separatedInputs[MAIN_COMMAND_INDEX]) {
+        case INPUT_HELP:
             commandType = CommandType.HELP;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_ADD)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_CLASS)) {
-            commandType = CommandType.ADD_CLASS;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_ADD)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_TEST)) {
-            commandType = CommandType.ADD_TEST;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_ADD)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_CCA)) {
-            commandType = CommandType.ADD_CCA;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_ADD)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_CONTACT)) {
-            commandType = CommandType.ADD_CONTACT;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_ADD)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_TUITION)) {
-            commandType = CommandType.ADD_TUITION;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DELETE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_CLASS)) {
-            commandType = CommandType.DELETE_CLASS;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DELETE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_TEST)) {
-            commandType = CommandType.DELETE_TEST;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DELETE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_CCA)) {
-            commandType = CommandType.DELETE_CCA;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DELETE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_QUIZ)) {
-            commandType = CommandType.DELETE_QUIZ;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DELETE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_CONTACT)) {
-            commandType = CommandType.DELETE_CONTACT;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_LIST)
-                && separatedInputs.length > 1
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_QUIZ)) {
-            commandType = CommandType.LIST_QUIZ;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_LIST)
-                && separatedInputs.length > 1
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_CONTACT)) {
-            commandType = CommandType.LIST_CONTACT;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_LIST)) {
+            break;
+        case INPUT_ADD:
+            commandType = CommandType.ADD;
+            break;
+        case INPUT_DELETE:
+            commandType = CommandType.DELETE;
+            break;
+        case INPUT_LIST:
             commandType = CommandType.LIST;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_BYE)) {
+            break;
+        case INPUT_BYE:
             commandType = CommandType.BYE;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DONE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_TEST)) {
-            commandType = CommandType.DONE_TEST;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DONE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_CLASS)) {
-            commandType = CommandType.DONE_CLASS;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_DONE)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_SCHEDULE_CCA)) {
-            commandType = CommandType.DONE_CCA;
-        } else if (separatedInputs[MAIN_COMMAND_INDEX].equals(INPUT_ADD)
-                && separatedInputs[SUB_COMMAND_INDEX].equals(INPUT_QUIZ)) {
-            commandType = CommandType.ADD_QUIZ;
-        } else {
+            break;
+        case INPUT_DONE:
+            commandType = CommandType.DONE;
+            break;
+        default:
             throw new InvalidCommandException();
         }
 
--- src/main/java/seedu/duke/controller/parser/CommandType.java
+++ src/main/java/seedu/duke/controller/parser/CommandType.java
@@ -4,8 +4,5 @@ package seedu.duke.controller.parser;
  * Represents all possible commands available to the user.
  */
 public enum CommandType {
-    ADD_CLASS, ADD_CCA, ADD_TEST, ADD_QUIZ, ADD_CONTACT, ADD_TUITION,
-    DELETE_CLASS, DELETE_CCA, DELETE_TEST, DELETE_QUIZ, DELETE_TUITION, DELETE_CONTACT,
-    DONE_TEST, DONE_CLASS, DONE_CCA, DONE_TUITION,
-    LIST_QUIZ, LIST_CONTACT, BYE, LIST, HELP
+    ADD, DELETE, DONE, HELP, LIST, BYE
 }
