b792d4a4ec01f8a86e09032c3492cea535ccc077
untitle4 <e0376922@u.nus.edu>
2020-10-18 23:30:22 +0800

    Add quiz exception

--- src/main/java/seedu/duke/controller/command/Command.java
+++ src/main/java/seedu/duke/controller/command/Command.java
@@ -1,6 +1,16 @@
 package seedu.duke.controller.command;
 
-import seedu.duke.exception.*;
+import seedu.duke.exception.CcaEmptyStringException;
+import seedu.duke.exception.CcaParamException;
+import seedu.duke.exception.ContactEmptyStringException;
+import seedu.duke.exception.ContactParamException;
+import seedu.duke.exception.EmptyTuitionInputException;
+import seedu.duke.exception.InvalidClassInputException;
+import seedu.duke.exception.InvalidHelpCommandException;
+import seedu.duke.exception.InvalidTuitionInputException;
+import seedu.duke.exception.QuizParamException;
+import seedu.duke.exception.TestEmptyStringException;
+import seedu.duke.exception.TestParamException;
 import seedu.duke.model.DataManager;
 
 public abstract class Command {
