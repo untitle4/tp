df604d5e464e496be62782f935efafe069cd06e3
AndreWongZH <wongandre98@gmail.com>
2020-10-26 14:21:36 +0800

    Add feature to notify about quiz
    If quiz has not been attempted in 2 or more days, the program will
    notify user that there are pending quizzes to attempt.
    
    Once the user attempt the quiz, the last attempted date will be
    updated.

--- src/main/java/seedu/duke/storage/QuizListDecoder.java
+++ src/main/java/seedu/duke/storage/QuizListDecoder.java
@@ -1,14 +1,30 @@
 package seedu.duke.storage;
 
+import dorkbox.notify.Notify;
 import seedu.duke.model.quiz.Quiz;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class QuizListDecoder {
     public ArrayList<Quiz> decodeQuizList(ArrayList<String> encodedQuizList) {
         final ArrayList<Quiz> decodedQuizzes = new ArrayList<>();
         for (String encodedQuiz : encodedQuizList) {
             decodedQuizzes.add(decodeQuizFromString(encodedQuiz));
+        }
+
+        for (Quiz quiz : decodedQuizzes) {
+            long numDays = DAYS.between(LocalDate.now(), quiz.getLastAccessed());
+            if (numDays <= -2) {
+                Notify.create()
+                        .title("Plan&score Notification")
+                        .text("you have outdated quizzes! Attempt them now!")
+                        .hideAfter(10000)
+                        .showInformation();
+                break;
+            }
         }
 
         return decodedQuizzes;
@@ -23,12 +39,9 @@ public class QuizListDecoder {
         String option3 = data[3];
         String option4 = data[4];
         String answer = data[5];
+        String explanation = data[6];
+        LocalDate lastAccessed = LocalDate.parse(data[7]);
 
-        if (data.length > 6) {
-            String explanation = data[6];
-            return new Quiz(question, option1, option2, option3, option4, answer, explanation);
-        }
-
-        return new Quiz(question, option1, option2, option3, option4, answer);
+        return new Quiz(question, option1, option2, option3, option4, answer, explanation, lastAccessed);
     }
 }
