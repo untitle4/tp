b00bad9ccc525fad0176c8a3203a1649f5f84ed4
AndreWongZH <wongandre98@gmail.com>
2020-10-27 14:56:44 +0800

    Update failing test

--- src/test/java/seedu/duke/controller/parser/CommandParserTest.java
+++ src/test/java/seedu/duke/controller/parser/CommandParserTest.java
@@ -2,6 +2,7 @@ package seedu.duke.controller.parser;
 
 import org.junit.jupiter.api.Test;
 import seedu.duke.exception.InvalidCommandException;
+import seedu.duke.exception.MissingModelException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 //@@author AndreWongZH
 class CommandParserTest {
     @Test
-    void extractCommand_deleteString_deleteCommandType() throws InvalidCommandException {
+    void extractCommand_deleteString_deleteCommandType() throws InvalidCommandException, MissingModelException {
         CommandType actualType = new CommandParser("delete 22").extractCommand();
         assertEquals(CommandType.DELETE, actualType);
     }
@@ -17,7 +18,7 @@ class CommandParserTest {
     @Test
     void extractCommand_singleDoneString_expectException() {
         CommandParser commandParser = new CommandParser("done");
-        assertThrows(InvalidCommandException.class, commandParser::extractCommand);
+        assertThrows(MissingModelException.class, commandParser::extractCommand);
     }
 
     @Test
