cf7641810a6f8786f3be6c684f0779b97b5d6b18
AndreWongZH <wongandre98@gmail.com>
2020-11-06 15:07:43 +0800

    Update javadocs

--- src/main/java/seedu/duke/ui/UserInterface.java
+++ src/main/java/seedu/duke/ui/UserInterface.java
@@ -53,6 +53,12 @@ public class UserInterface {
     }
 
     //@@author AndreWongZH
+    /**
+     * Prints to user through an array list of strings.
+     * This is invoked from either list or find command.
+     *
+     * @param stringArrayList ArrayList of type string to be printed.
+     */
     public void printArray(ArrayList<String> stringArrayList) {
         assert stringArrayList != null;
         for (String line : stringArrayList) {
@@ -61,11 +67,25 @@ public class UserInterface {
     }
 
     //@@author AndreWongZH
+    /**
+     * Compares if command type is equal to BYE and returns true if it does.
+     *
+     * @param commandType A CommandType of one of the commands.
+     * @return A boolean to indicate if program loop should end.
+     */
     public boolean checkIfProgramEnds(CommandType commandType) {
         return commandType != CommandType.BYE;
     }
 
     //@@author AndreWongZH
+    /**
+     * Passes user input into the control manager for it to run its logic.
+     *
+     * @param model Data stored in the program.
+     * @param eventStorageManager Updates the storage data in event.txt after command is ran.
+     * @param quizStorageManager Updates the storage data in quiz.txt after command is ran.
+     * @return A boolean to tell the program to quit or not.
+     */
     public boolean runUI(Model model,
                          EventStorageManager eventStorageManager,
                          QuizStorageManager quizStorageManager) {
