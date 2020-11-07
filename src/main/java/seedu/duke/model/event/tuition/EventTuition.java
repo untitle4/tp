374a6d6cae25b6b0b0f12e8578f5c6a79cf12d04
chanx <chanxuhui@outlook.com>
2020-11-07 17:51:06 +0800

    Add JavaDocs for EventTuition.java

--- src/main/java/seedu/duke/model/event/tuition/EventTuition.java
+++ src/main/java/seedu/duke/model/event/tuition/EventTuition.java
@@ -7,23 +7,45 @@ import seedu.duke.controller.parser.DateTimeParser;
 import java.util.Calendar;
 
 //@@author durianpancakes
+
+/**
+ * Class EventTuition
+ * Contains constructors, getters, toString and equals methods.
+ */
 public class EventTuition extends Event {
     public static final String TUITION_ICON = "[TUITION]";
     private final String location;
 
+    /**
+     * Constructs a EventTuition with a default NOT DONE status
+     *
+     * @param description String containing description of the EventTuition
+     * @param start Calendar containing the start time of the EventTuition
+     * @param end Calendar containing the end time of the EventTuition
+     * @param location String containing the location of the EventTuition
+     */
     public EventTuition(String description, Calendar start, Calendar end, String location) {
         super(description, start, end);
+        this.location = location;
+    }
+
+    /**
+     * Constructs a EventTuition with a given NOT DONE/DONE status
+     *
+     * @param description String containing description of the EventTuition
+     * @param isDone boolean containing the done status of the EventTuition
+     * @param start Calendar containing the start time of the EventTuition
+     * @param end Calendar containing the end time of the EventTuition
+     * @param location String containing the location of the EventTuition
+     */
+    public EventTuition(String description, boolean isDone, Calendar start, Calendar end, String location) {
+        super(description, start, end, isDone);
         this.location = location;
     }
 
     @Override
     public String getIcon() {
         return TUITION_ICON;
-    }
-
-    public EventTuition(String description, boolean isDone, Calendar start, Calendar end, String location) {
-        super(description, start, end, isDone);
-        this.location = location;
     }
 
     public String getLocation() {
