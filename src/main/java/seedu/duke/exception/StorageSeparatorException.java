f46b221a256f90cf51417cbbca0124181a0eeb28
chanx <chanxuhui@outlook.com>
2020-11-09 20:29:22 +0800

    Update code to match coding standards

--- src/main/java/seedu/duke/exception/StorageSeparatorException.java
+++ src/main/java/seedu/duke/exception/StorageSeparatorException.java
@@ -1,7 +1,7 @@
 package seedu.duke.exception;
 
 /**
- * Represents the error when the separator used between each parameter of an Event is not "|"
+ * Represents the error when the separator used between each parameter of an Event is not "|".
  */
 public class StorageSeparatorException extends Exception {
 }
